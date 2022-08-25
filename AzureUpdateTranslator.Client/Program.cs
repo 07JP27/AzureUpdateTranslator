54796769664a1344c1b19a3fc058bc5be444f5c2
07JP27 <jynpei.t@gmail.com>
2022-08-25 19:06:20 +0900

    中間コミット

--- AzureUpdateTranslator.Client/Program.cs
+++ AzureUpdateTranslator.Client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Immutable;
 using System.Text;
 using AzureUpdateTranslator.Share.Dtos;
 using Newtonsoft.Json;
@@ -7,6 +8,9 @@ namespace AzureUpdateTranslator.Client
 {
     public class Program
     {
+        private const int POLLING_INTERVAL = 5000;
+        private const string ENDPOINT = "http://localhost:7004/api/starter";
+
         static async Task Main(string[] i_args)
         {
             CommandLine.ParserResult<Options> result = CommandLine.Parser.Default.ParseArguments<Options>(i_args);
@@ -18,7 +22,7 @@ namespace AzureUpdateTranslator.Client
                 var inputFilePath = parsedOptions.Value.InputFile;
                 var noTranslation = parsedOptions.Value.NoTranslate;
 
-                System.Console.WriteLine($"Input: {inputFilePath}");
+                System.Console.WriteLine($"Target file: {inputFilePath}");
                 System.Console.WriteLine($"NoTranslate: {noTranslation}");
 
                 List<string> targetUrls = new List<string>();
@@ -33,7 +37,7 @@ namespace AzureUpdateTranslator.Client
                     }
                 }
 
-                System.Console.WriteLine($"Found {targetUrls.Count} URLs.");
+                System.Console.WriteLine($"{targetUrls.Count} URLs found.");
 
                 var requestDto = new RequestDto();
                 requestDto.Urls = targetUrls;
@@ -41,9 +45,42 @@ namespace AzureUpdateTranslator.Client
 
                 var client = new HttpClient();
                 var content = new StringContent(JsonConvert.SerializeObject(requestDto), Encoding.UTF8, "application/json");
-                var response = await client.PostAsync("hogehoge", content);
+                var response = await client.PostAsync(ENDPOINT, content);
                 response.EnsureSuccessStatusCode();
                 var checkStatus = JsonConvert.DeserializeObject<CheckStatusResponseDto>(await response.Content.ReadAsStringAsync());
+                Console.WriteLine($"StatusURL:{checkStatus.StatusQueryGetUri}");
+
+                var loopCondition = ImmutableList.Create("Pending", "Running");
+                StatusResponseDto status;
+
+                while (true)
+                {
+                    var statusRes = await client.GetAsync(checkStatus.StatusQueryGetUri + "&showInput=false");
+                    status = JsonConvert.DeserializeObject<StatusResponseDto>(await statusRes.Content.ReadAsStringAsync());
+                    Console.WriteLine($"Status:{status.RuntimeStatus}");
+
+                    if (!loopCondition.Contains(status.RuntimeStatus)) break;
+                    await Task.Delay(POLLING_INTERVAL);
+                }
+                
+                if(status.RuntimeStatus == "Completed")
+                {
+                    using (StreamWriter writer = new StreamWriter("output.md", false))
+                    {
+                        foreach(var topic in status.Output)
+                        {
+                            await writer.WriteAsync(topic);
+                            await writer.WriteAsync("---");
+                        }
+                    }
+
+                    Console.WriteLine($"Completed");
+                }
+                else
+                {
+                    Console.Error.WriteLine(status.Output);
+                }
+               
             }
 
             System.Console.ReadKey();
---
+++ AzureUpdateTranslator.Share/Dtos/StatusResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureUpdateTranslator.Share.Dtos
+{
+    public class StatusResponseDto
+    {
+        public string RuntimeStatus { get; set; } = string.Empty;
+        public List<string> Output { get; set; } = new List<string>();
+    }
+}
